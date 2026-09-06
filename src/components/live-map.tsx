5f023e0fd3a4d080e3fe0d449bea1f8f7b84b988
Shan Usmani <takhiro79@gmail.com>
2026-09-06 22:01:50 +0530

    fix: revert map tiles from CARTO dark back to OSM

--- src/components/live-map.tsx
+++ src/components/live-map.tsx
@@ -424,7 +424,7 @@ export function LiveMap() {
         <ZoomWatcher onZoom={setZoom} />
         <MapClickHandler onPick={handlePick} />
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
         />
         <ZoomControl position="bottomright" />
--- src/components/rescuer-map.tsx
+++ src/components/rescuer-map.tsx
@@ -204,7 +204,7 @@ export function RescuerMap() {
           >
             <FlyTo target={center} />
             <TileLayer
-              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
             />
 
